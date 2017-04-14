5a278232a47b17ffb0f35921b5ce58791f432102
Mal <mbrunson84@gmail.com>
2017-04-13 20:51:01 -0400

    Final commit

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
+var num1;
+var num2;
+var res;
+var results;
+
+document.addEventListener("DOMContentLoaded", function(){
+	num1 = document.getElementById("num1");
+	num2 = document.getElementById("num2");
+	res = document.getElementById("result");
+	console.log(num1);
+	console.log(num2);
+	console.log(res);
+});
+
 function add(num1, num2){
-	var result = null;
-	console.log(result);
+	result = +num1.value + +num2.value;
+	res.value = result;
+	
 }
 function subtract(num1, num2){
-	var result = null;
-	console.log(result);
+	result = +num1.value - +num2.value;
+	res.value = result;
 }
 function multiply(num1, num2){
-	var result = null;
-	console.log(result);
+	result = +num1.value * +num2.value;
+	res.value = result;
 }
 function divide(num1, num2){
-	var result = null;
-	console.log(result);
+	result = +num1.value / +num2.value;
+	res.value = result;
 }
